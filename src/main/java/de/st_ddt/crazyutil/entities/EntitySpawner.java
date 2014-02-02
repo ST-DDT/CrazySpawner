3c4e0ed7776e22004a6c5f2891c53bbbdffbdf1d
ST-DDT <ST-DDT@gmx.de>
2014-02-02 11:32:46 +0100

    Added javadocs to EntitySpawner

--- src/main/java/de/st_ddt/crazyutil/entities/EntitySpawner.java
+++ src/main/java/de/st_ddt/crazyutil/entities/EntitySpawner.java
@@ -4,13 +4,24 @@ import org.bukkit.Location;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 
+/**
+ * Classes implementing this Interface are Entity Spawners.
+ */
 public interface EntitySpawner extends EntityMatcher
 {
 
 	public final static EntitySpawner[] ENTITYSPAWNERS = new EntitySpawner[EntityType.values().length];
 
+	/**
+	 * @return The {@link EntityType} spawned by this Spawner.<br>
+	 *         If it does spawn more than one {@link EntityType}, this method return Null.
+	 */
 	public EntityType getEntityType();
 
+	/**
+	 * @return The highest entity Class shared with all possible spawned entities.<br>
+	 *         If this Spawns only one {@link EntityType} this method returns {@link EntityType#getEntityClass()}.
+	 */
 	public Class<? extends Entity> getEntityClass();
 
 	public EntitySpawnerType getSpawnerType();
