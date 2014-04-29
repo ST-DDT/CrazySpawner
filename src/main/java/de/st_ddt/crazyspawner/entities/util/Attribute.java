fab80e7c8b9d589eddfc9533c267311cc4ef68e7
ST-DDT <ST-DDT@gmx.de>
2014-04-29 18:50:56 +0200

    Added min/max values to attributes

--- src/main/java/de/st_ddt/crazyspawner/entities/util/Attribute.java
+++ src/main/java/de/st_ddt/crazyspawner/entities/util/Attribute.java
@@ -5,7 +5,41 @@ package de.st_ddt.crazyspawner.entities.util;
  */
 public enum Attribute
 {
-	FOLLOW_RANGE,
-	MOVEMENT_SPEED,
-	KNOCKBACK_RESISTANCE;
+	FOLLOW_RANGE(0),
+	MOVEMENT_SPEED(0),
+	KNOCKBACK_RESISTANCE(0);
+
+	private double min;
+	private double max;
+
+	private Attribute(final double min)
+	{
+		this(min, Double.MAX_VALUE);
+	}
+
+	private Attribute(final double min, final double max)
+	{
+		this.min = min;
+		this.max = max;
+	}
+
+	public final double getMin()
+	{
+		return min;
+	}
+
+	public final double getMax()
+	{
+		return max;
+	}
+
+	public double filter(final double value)
+	{
+		return Math.max(min, Math.min(max, value));
+	}
+
+	public String shortName()
+	{
+		return name().replaceAll("_", "");
+	}
 }
