b979cfafa01a10a9ef254c9272eb158f6c83624d
ST-DDT <ST-DDT@gmx.de>
2014-07-02 21:58:01 +0200

    Added some javadocs

--- src/main/java/de/st_ddt/crazyutil/entities/EntitySpawnerType.java
+++ src/main/java/de/st_ddt/crazyutil/entities/EntitySpawnerType.java
@@ -1,10 +1,28 @@
 package de.st_ddt.crazyutil.entities;
 
+/**
+ * A list of possible EntitySpawner types.
+ */
 public enum EntitySpawnerType
 {
+	/**
+	 * Raw Spawner, very basic none configurable spawner, cannot have parents.
+	 */
 	RAW,
+	/**
+	 * Special Spawner, spawner is {@link ConfigurableEntitySpawner}, cannot have parents, cannot be replaced by a {@link #CONFIGURABLE}
+	 */
 	SPECIAL,
+	/**
+	 * Configurable Spawner, spawner is {@link ConfigurableEntitySpawner}.
+	 */
 	CONFIGURABLE,
+	/**
+	 * Named Spawner, spawner is {@link NamedEntitySpawner}.
+	 */
 	NAMED,
+	/**
+	 * Random Spawner, spawns a Random Entity (based on setup).
+	 */
 	RANDOM,
 }
